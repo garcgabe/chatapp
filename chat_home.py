061ffbd631e4f0b0281e8f5ddf7c77e51f77c3fd
garcgabe <garciagabe0033@yahoo.com>
2023-09-10 11:20:50 -0400

    baseline:

--- chat_home.py
+++ chat_home.py
@@ -1,8 +1,27 @@
 import streamlit as st
 import numpy as np
 
-st.sidebar.markdown("homepage")
+st.sidebar.markdown("tutor")
 
-st.title("homepage of gabe's library management")
+st.title("welcome to gpt")
+
+left, right = st.columns(2, gap = "medium")
+
+with left: 
+    st.subheader("you")
+    st.write("gabe")
+    st.subheader("response:")
+
+with right:
+    st.subheader("gpt")
+    st.write("mr. chat")
+
+
+write_tab = st.tabs(['write-back'])
+
+with write_tab:
+    reflection = st.text_area("speak here")
+    if st.button("send text"):
+        x = "y"
 
 
