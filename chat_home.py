466c776e0f43ab647305f425bf835c7cdf20b06d
garcgabe <garciagabe0033@yahoo.com>
2023-09-12 09:15:28 -0400

    baseline:

--- chat_home.py
+++ chat_home.py
@@ -17,11 +17,11 @@ with right:
     st.write("mr. chat")
 
 
-write_tab = st.tabs(['write-back'])
+# write_tab = st.tabs(['write-back'])
 
-with write_tab:
-    reflection = st.text_area("speak here")
-    if st.button("send text"):
-        x = "y"
+# with write_tab:
+#     reflection = st.text_area("speak here")
+#     if st.button("send text"):
+#         x = "y"
 
 
